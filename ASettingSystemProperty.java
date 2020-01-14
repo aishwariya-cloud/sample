6abbedcc5accbf80b495fa716caa4020d5376d4c
aishwariya-cloud <59167955+aishwariya-cloud@users.noreply.github.com>
2020-01-14 16:37:39 +0530

    Add files via upload

---
+++ ASettingSystemProperty.java
@@ -0,0 +1,23 @@
+package ASeleniumDeloitteDemoPackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ASettingSystemProperty {
+
+	public static void main(String[] args) {
+	
+		System.setProperty("webdriver.chrome.driver","C:\\software testing\\selenium\\chromedriver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://LinkedIn.com");
+		//to max the screen
+		driver.manage().window().maximize();
+		//finding the element using the locator
+		driver.findElement(By.name("session_key")).sendKeys("aishwariyadharmaraj47@gmail.com");
+		driver.findElement(By.name("session_password")).sendKeys("Dairymilk@2829");
+
+
+	}
+
+}
